b4e1fbe68e38d83b7e3a190f3f986a199f6407ce
William Avila <william.zx1@unitec.edu>
2012-07-20 09:50:14 -0600

    agregar clase

---
+++ src/programacion2_2_1_2012/Wiilliam.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package programacion2_2_1_2012;
+
+/**
+ *
+ * @author William
+ */
+public class Wiilliam {
+   int x;
+   String txt;
+}
